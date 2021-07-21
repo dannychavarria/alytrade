64796e09991f554f18d5d2d0e1f06ea72f184cce
JosephDBX <st.joseph.dbx@gmail.com>
2021-07-21 11:10:56 -0600

    :sparkles: - update-login-container - Steven Bustillo - login view design added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 import alyTradeOrbe from './img/Alytrade_Orbe.svg'
+import background from './img/background.png'
+import alyTradeLogo from './img/Alytrade_Logo.svg'
+import poweredBy from './img/poweredBy.svg'
 
-export { alyTradeOrbe }
+export { alyTradeOrbe, background, alyTradeLogo, poweredBy }
